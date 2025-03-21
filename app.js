148e54d5e3b8b5a26969b455df2eda60ae282937
Rebiya <rebum.19@gmail.com>
2025-03-21 18:26:07 +0300

    app js updated to allow all origin

--- app.js
+++ app.js
@@ -4,24 +4,19 @@ require("dotenv").config();
 const query = require("./Config/db.config");
 const cors = require("cors");
 const expressSanitizer = require("express-sanitizer"); // Fixed sanitization issue
+
 // Create an Express application
 const app = express();
-// CORS configuration to handle cross-origin requests
-const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
+
+// Allow all origins
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*", // Allow all origins
   credentials: true, // Allows cookies and auth headers
   optionsSuccessStatus: 200,
 };
 
 // Apply middleware
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); // Apply CORS with the updated configuration
 app.use(express.json());
 app.use(expressSanitizer()); // Fix: Correct sanitizer middleware
 
